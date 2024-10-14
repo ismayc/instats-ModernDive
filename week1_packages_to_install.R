8c3d8c5d1bbced372b02777777bbce88fe924ff7
Chester Ismay <chester.ismay@gmail.com>
2024-10-13 20:23:26 -0700

    Package updates

---
+++ week1_packages_to_install.R
@@ -0,0 +1,8 @@
+# This is a vector of the packages that you will need to install for the 
+# first week of the course.
+
+week1_packages <- c("dplyr", "fivethirtyeight", "ggplot2", "knitr", "lubridate",
+                    "moderndive", "palmerpenguins", "readr", "rmarkdown",
+                    "tibble", "tidyr")
+
+install.packages(week1_packages, repos = "https://cran.rstudio.com")
